0a7a40897f869f3de782c1ae98f5016ca22981e9
PhinehasBerko <phinehasdscience@gmail.com>
2026-01-26 11:32:48 +0000

    build: interactive counter app

---
+++ JS/counter_app.js
@@ -0,0 +1,29 @@
+const { jsx } = require("react/jsx-runtime");
+
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+const incrementBtn = document.getElementById("add");
+const decrementBtn = document.getElementById("subtract");
+const resetBtn = document.getElementById("reset");
+
+function updateUI() {
+  countDisplay.textContent = count;
+}
+
+incrementBtn.addEventListener("click", () => {
+    count++;
+    updateUI();
+});
+
+decrementBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    updateUI();
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  updateUI();
+});
